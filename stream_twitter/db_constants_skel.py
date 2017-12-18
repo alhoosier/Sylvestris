2853b0407552e839228f693387e91cc543d7e0e0
Aaron <alhoosier@gmail.com>
2017-12-17 18:29:39 -0800

    Refactored stream_twitter for easier reuse and added local_utils module
    Encapsulated all functions within classes where appropriate. Moved generic functions to a local_utils module. Replaced YAML connection files with python constants files.

---
+++ stream_twitter/db_constants_skel.py
@@ -0,0 +1,6 @@
+# CONST_DB_HOST = # DB Host
+# CONST_DB_PORT = # DB Port
+# CONST_DB_NAME = # DB Name
+# CONST_DB_SSL_MODE = # DB SSL Mode (require, verify, etc.)
+# CONST_DB_USER = # DB Username
+# CONST_DB_PASS = # DB User Password
